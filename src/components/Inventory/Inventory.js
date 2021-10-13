3440ce2970a898469c4a7f4178ccca7120c56b75
hablu-rohan <belaluddi100396@gmail.com>
2021-10-13 12:07:52 -0700

    updated the inventory page

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Inventory() {
     return (
         <div>
-            <h2>This is inventory</h2>
+            <h2>This is inventory page</h2>
         </div>
     )
 }
